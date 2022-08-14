18639199840d0029bfd22536faf013d13a1707ba
Billi <55185706+billylebogang@users.noreply.github.com>
2022-08-14 19:17:13 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,46 +2,60 @@ import flask
 from flask import Flask, render_template, json, jsonify, request
 import os
 import numpy as np
-import helper
-from helper import preprocessdata, get_test_json
+import pandas as pd
+import joblib
+import sklearn
 
 app = Flask(__name__)
 
 
-#testing the prediction model
+def preprocessdata( gender, married, education, self_employed, applicant_income, co_applicant_income, loan_amount, loan_amount_term, credit_history, property_area):
+
+    if (gender.lower() == 'male'):
+        gender = 1
+    else:
+        gender == 0 
+   
+    if (married.lower()  == 'yes'):
+        married = 1
+    else:
+        married = 0 
+
+    if (education.lower()  == 'graduate'):
+        education = 1
+    else:
+        education = 0 
+
+    if (self_employed.lower()  == 'yes'):
+        self_employed = 1
+    else:
+        self_employed = 0 
+
+    if (property_area.lower()  == 'urban'):
+        property_area = 1
+    else:
+        property_area = 0 
+
+    test_data = [[gender, married, education, self_employed, applicant_income, co_applicant_income, loan_amount, loan_amount_term, credit_history, property_area]]
+
+    trained_model = joblib.load('./model_1.pkl') #loading the model
+
+    prediction = trained_model.predict(test_data) # making a prediction
+
+    return prediction
 
 
-
-
-
-
+def get_test_json():
+    df = pd.read_csv("./data/train.csv")
+    return(df.head())
 
 #testing the the end point data
-
-data_ = [{
-	"id":0,
-	"name":"billy",
-	"surname":"lebogang"
-}, 
-{
-	"id":1,
-	"name":"Nonofo",
-	"surname":"Mathambo"
-},
-{
-	"id":2,
-	"name":"Popo",
-	"surname":"lebogang"
-}
-
-]
 
 test_data = get_test_json()
 
 @app.route('/') # my home end point returns the main index
 def home():
 	return render_template('index.html')
-
 
 
 
